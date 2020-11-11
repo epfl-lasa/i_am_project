86e856904cd120bd41f0e1b52469ad9571e66c8e
harshitk95 <harshit.khurana@epfl.ch>
2020-11-11 11:14:54 +0100

    Added the functions without the Eigen supprt for basic DS made by Jari TU/e

--- include/tracking_velocity.h
+++ include/tracking_velocity.h
@@ -4,6 +4,8 @@
 #include <cstdio>
 #include <iostream>
 
+#include <numeric>
+
 
 #include "math.h"
 
@@ -13,5 +15,5 @@
 
 
 float calculate_distance(std::vector<float> point1, std::vector<float> point2);
-void get_position_in_line(std::vector<float> position_in_line, std::vector<float> desired_hitting_position, std::vector<float> iiwa_end_effector_position);
+void get_position_in_line(std::vector<float> &position_in_line, std::vector<float> desired_hitting_position, std::vector<float> end_effector_position,  std::vector<float> end_effector_position, std::vector<float> desired_impact_velocity);
 void voila();
--- src/tracking_velocity.cpp
+++ src/tracking_velocity.cpp
@@ -2,11 +2,19 @@
 
 
 float calculate_distance(std::vector<float> point1, std::vector<float> point2){
-    
-    return 0;
+    float sum = 0;
+
+    for (unsigned int i = 0; i < point1.size(); i++){
+        sum += (point1[i] - point2[i])*(point1[i] - point2[i]);
+    }
+    return sqrt(sum);
 }
 
-void get_position_in_line(std::vector<float> position_in_line, std::vector<float> desired_hitting_position, std::vector<float> iiwa_end_effector_position){
+void get_position_in_line(std::vector<float> &position_in_line, std::vector<float> desired_hitting_position, std::vector<float> end_effector_position, std::vector<float> desired_impact_velocity){
+    int length = position_in_line.size();
+
+    float hitting_speed = std::inner_product(desired_impact_velocity.begin(), desired_impact_velocity.end(), desired_impact_velocity.begin(), 0);
+    position_in_line = desired_hitting_position - (d/hitting_speed)*desired_impact_velocity;
 
 }
 

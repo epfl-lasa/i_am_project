d3814e782d244ae71cf05ce47242194cd055994b
harshitk95 <harshit.khurana@epfl.ch>
2022-09-22 14:18:22 +0200

    need the QP

---
+++ include/optimizer.h
@@ -0,0 +1,10 @@
+#include <osqp/osqp.h>
+
+#include <OsqpEigen/Solver.hpp>
+#include <cstdio>
+#include <iostream>
+#include <numeric>
+#include "math.h"
+#include <vector>
+#include <string>
+#include <Eigen/Dense>

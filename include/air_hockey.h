dce515e4de2a294c7b8e2ae2e38b9121ce59f99d
Elise-J <elise.jeandupeux@gmail.com>
2023-05-08 18:24:36 +0200

    cleaning comments

--- include/air_hockey.h
+++ include/air_hockey.h
@@ -1,39 +1,35 @@
 #pragma once
 
+#include "move_robot.h"
 #include "ros/package.h"
 #include "ros/ros.h"
 #include "thirdparty/Utils.h"
+#include "tools/hittable.h"
+#include "tools/quattools.h"
 #include <ros/console.h>
-
-#include <cmath>
-#include <cstdio>
-#include <fstream>
-#include <iostream>
-#include <math.h>
-#include <numeric>
-#include <queue>
-#include <sstream>
-#include <string>
-#include <vector>
 
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
+#include <cmath>
+#include <cstdio>
+#include <fstream>
 #include <gazebo_msgs/LinkStates.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <gazebo_msgs/SetModelState.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
+#include <iostream>
+#include <math.h>
+#include <numeric>
+#include <queue>
 #include <sensor_msgs/JointState.h>
+#include <sstream>
 #include <std_msgs/Float64.h>
 #include <std_msgs/Float64MultiArray.h>
 #include <std_msgs/Int16.h>
-
-#include "move_robot.h"
-
-#include "tools/hittable.h"
-// #include "../include/modeselektor.h"
-#include "tools/quattools.h"
+#include <string>
+#include <vector>
 
 #include <experimental/filesystem>
 #define _USE_MATH_DEFINES
--- include/tools/quattools.h
+++ include/tools/quattools.h
@@ -5,7 +5,14 @@
 #include <math.h>
 #include <numeric>
 
+// Quaternion multiplication a*b
 Eigen::Vector4d quatProd(Eigen::Vector4d a, Eigen::Vector4d b);
+
+// Quaternion to euler angle
 Eigen::Vector3d quatToRPY(Eigen::Vector4d q);
+
+// Euler angle to quaternion
 Eigen::Vector4d rpyToQuat(double r, double p, double y);
+
+// Computer quaternion from line that goes from point_1 to point_2
 Eigen::Vector4d pointsToQuat(Eigen::Vector3d point_1, Eigen::Vector3d point_2);

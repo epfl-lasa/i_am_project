c5d6dda96449be37a556d4fb85ab5d424c6ff74c
Elise-J <elise.jeandupeux@gmail.com>
2023-09-25 15:42:00 +0200

    adding full inertia feature possibility

--- docker/build-server.sh
+++ docker/build-server.sh
@@ -14,9 +14,7 @@ Build a Docker container for remote development and/or running unittests.
 Options:
   --base-tag               The tag of ros2-control-libraries image.
 
-  -b|--branch-iiwa-toolkit Branch name for the iiwa-toolkit (either feature_ns_inertial_control or feature_ns_full_inertia) (ONLY IF INERTIA FLAG IS USED!)
-
-  -i|--inertia             Build docker based on iiwa_ros feature/inertia and use iiwa_toolkit controller
+  -b|--branch-iiwa-toolkit Branch name for the iiwa-toolkit (either feature_ns_inertial_control or feature_ns_full_inertia)
 
   -r, --rebuild            Rebuild the image with no cache.
 
@@ -33,7 +31,6 @@ while [ "$#" -gt 0 ]; do
   case "$1" in
     --base-tag) BASE_IMAGE_TAG=$2; shift 2;;
     -b|--branch-iiwa-toolkit) IIWA_TOOLKIT_BRANCH=$2; shift 2;;
-    -i|--inertia) INERTIA=true; shift 1;;
     -r|--rebuild) BUILD_FLAGS+=(--no-cache); shift 1;;
     -v|--verbose) BUILD_FLAGS+=(--progress=plain); shift 1;;
     -s|--serve) SERVE_REMOTE=true ; shift ;;
@@ -49,12 +46,7 @@ BUILD_FLAGS+=(-t "${IMAGE_NAME}:${BASE_IMAGE_TAG}")
 BUILD_FLAGS+=(--build-arg HOST_GID=$(id -g))   # Pass the correct GID to avoid issues with mounted volumes
 BUILD_FLAGS+=(--ssh default="${SSH_AUTH_SOCK}") # Pass git ssh key to be able to pull
 
-
-if [ "${INERTIA}" = true ]; then
-  DOCKER_BUILDKIT=1 docker build "${BUILD_FLAGS[@]}" -f ./docker/Dockerfile_inertia .
-else
 DOCKER_BUILDKIT=1 docker build "${BUILD_FLAGS[@]}" -f ./docker/Dockerfile .
-fi
 
 if [ "${SERVE_REMOTE}" = true ]; then
   aica-docker server "${IMAGE_NAME}:${BASE_IMAGE_TAG}" -u ros -p "${REMOTE_SSH_PORT}"

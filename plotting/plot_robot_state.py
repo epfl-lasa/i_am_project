7b4e847145c51e2f6e7c0d5e4e94529db899586f
Maxime00 <maxime.gautier@epfl.ch>
2024-01-26 17:28:45 +0100

    minor fixes for real demo

--- plotting/plot_robot_state.py
+++ plotting/plot_robot_state.py
@@ -128,13 +128,13 @@ if __name__== "__main__" :
 
     # path_to_data_airhockey = "/home/ros/ros_ws/src/i_am_project/data/airhockey/"
     path_to_data_airhockey = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/data/airhockey/"
-    hit_number = 3
+    hit_number = 19
         
     # Example usage
     # process_timestamped_folders('/path/to/timestamped_folders')
 
     # test one plot
-    path_to_robot_hit = path_to_data_airhockey + f"2024-01-11_15:57:49/IIWA_14_hit_{hit_number}.csv"
-    path_to_object_hit = path_to_data_airhockey + f"2024-01-11_15:57:49/object_hit_{hit_number}.csv"
+    path_to_robot_hit = path_to_data_airhockey + f"2024-01-26_16:03:41/IIWA_7_hit_{hit_number}.csv"
+    path_to_object_hit = path_to_data_airhockey + f"2024-01-26_16:03:41/object_hit_{hit_number}.csv"
     plot_robot_data(path_to_robot_hit, show_plot=False)
     plot_object_data(path_to_object_hit)
